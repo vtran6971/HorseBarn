7caa23947a1493bc8b7744052a743cee7064957a
Vincent Tran <vtran6971@bths.edu>
2018-12-02 21:25:27 -0500

    progress

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -1,2 +1,27 @@
+import com.company.Horse;
+
 public class HorseBarn {
+    private Horse[] spaces;
+    public HorseBarn(int stalls)
+    {
+     spaces = new Horse[stalls];
+    }
+
+    public int findHorseSpace(String Name)
+    {
+        for (int i = 0; i < this.spaces.length; i++)
+        {
+            if (this.spaces[i]!=null && Name.equals(this.spaces[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public  String toString()
+    {
+        String output = "";
+        output += this.getName()+","+" weights: " +this.getWeight();
+        return output;
+    }
 }
